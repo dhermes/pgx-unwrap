9ff77b5268635e733a9b93ba0ec545fa41c83f46
Danny Hermes <daniel.j.hermes@gmail.com>
2024-07-06 17:01:59 -0500

    Map out implementation in the form of a unit test.

--- tx_test.go
+++ tx_test.go
@@ -13,10 +13,15 @@
 package unwrap_test
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"reflect"
 	"testing"
+	"unsafe"
 
+	pgx "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	testifyrequire "github.com/stretchr/testify/require"
 
 	unwrap "github.com/dhermes/pgx-unwrap"
@@ -26,8 +31,77 @@ func TestExtractTx(t *testing.T) {
 	t.Parallel()
 	assert := testifyrequire.New(t)
 
-	var tx *sql.Tx
+	pool, err := sql.Open("pgx", "postgres://...")
+	assert.Nil(err)
+	t.Cleanup(func() {
+		err := pool.Close()
+		assert.Nil(err)
+	})
+
+	ctx := context.Background()
+	tx, err := pool.BeginTx(ctx, nil)
+	assert.Nil(err)
+	t.Cleanup(func() {
+		err := tx.Rollback()
+		assert.Nil(err)
+	})
+
 	unwrapped, err := unwrap.ExtractTx(tx)
 	assert.Nil(unwrapped)
 	assert.Equal("not implemented", fmt.Sprintf("%v", err))
+
+	// 1.
+	txValue := reflect.ValueOf(tx)
+	assert.Equal(reflect.Pointer, txValue.Type().Kind())
+
+	// 2.
+	txValue = txValue.Elem()
+	assert.Equal(reflect.Struct, txValue.Type().Kind())
+
+	// 3.
+	txiValue := txValue.FieldByName("txi")
+	assert.Equal(reflect.Interface, txiValue.Type().Kind())
+	assert.True(txiValue.CanAddr())
+	assert.False(txiValue.CanSet())
+	assert.False(txiValue.CanInterface())
+
+	// 4.
+	txiValue, copied := unwrap.CopyReflectData(txiValue)
+	assert.True(copied)
+	assert.Equal(reflect.Interface, txiValue.Type().Kind())
+	assert.True(txiValue.CanAddr())
+	assert.True(txiValue.CanSet())
+	assert.True(txiValue.CanInterface())
+
+	// 5.
+	wrapTxValue := reflect.ValueOf(txiValue.Interface())
+	assert.Equal(reflect.Struct, wrapTxValue.Type().Kind())
+	assert.False(wrapTxValue.CanAddr())
+	assert.False(wrapTxValue.CanSet())
+	assert.True(wrapTxValue.CanInterface())
+
+	// 6.
+	wrapTxValue = unwrap.CopyReflectValue(wrapTxValue)
+	assert.Equal(reflect.Struct, wrapTxValue.Type().Kind())
+	assert.True(wrapTxValue.CanAddr())
+	assert.True(wrapTxValue.CanSet())
+	assert.True(wrapTxValue.CanInterface())
+
+	// 7.
+	wrapTxType := wrapTxValue.Type()
+	assert.Equal("github.com/jackc/pgx/v5/stdlib", wrapTxType.PkgPath())
+	assert.Equal("wrapTx", wrapTxType.Name())
+
+	// 8.
+	p := unsafe.Pointer(wrapTxValue.UnsafeAddr())
+	wt := (*wrapTx)(p)
+	assert.NotNil(wt)
+	assert.NotNil(wt.tx)
+}
+
+// wrapTx is vendored in from the pgx source:
+// https://github.com/jackc/pgx/blob/v5.6.0/stdlib/sql.go#L874-L877
+type wrapTx struct {
+	ctx context.Context
+	tx  pgx.Tx
 }
